1939dadd58315fc80bdaa269e3b5d7a4bbc2964c
Ahmet Sivri <ahmet.sivri@cleverspeck.com>
2018-08-01 12:37:32 -0400

    commit

---
+++ components/SearchBar.vue
@@ -0,0 +1,20 @@
+<template>
+
+    <input @input="onSearchBar" /> <!-- @ or v-on used for listening DOM elements. -->
+
+</template>
+
+<script>
+    export default {
+        methods: {
+            onSearchBar: function(event)
+            {
+                this.$emit('newSearch', event.target.value);
+            }
+        }
+    }
+</script>
+
+
+<style>
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,41 @@
+<template>
+   <div id="app">
+      <h3>Welcome to the Paper-Searcher | An experimental challenge for the day. </h3>
+      <SearchBar v-on:newSearch="searchChanged"></SearchBar>
+  </div>
+</template>
+
+<script>
+import axios from 'axios';
+  import SearchBar from "~/components/SearchBar.vue";
+  const API_KEY = "cde616a7d0b956c3f58a2542ba740e06"; //API Key for ScienceDirect
+
+  export default {
+    name: 'app',
+    components: {
+      SearchBar
+    },
+    methods: {
+        searchChanged: function(querySearch)
+        {
+          console.log("In the function!");
+          axios.get("https://api.elsevier.com/content/search/scidir", {
+            params: {
+            query: querySearch,
+            apiKey: API_KEY
+          }
+          }).then(function(response){
+            console.log(response);
+          })
+          .catch(function(error){
+            console.log(error);
+          })
+        }
+    }
+  }
+
+</script>
+
+<style>
+</style>
+
